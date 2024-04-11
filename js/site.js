0b52d803da309fe4a458a17b1dd1ff016f45d18f
Sawyer Dillon <162613552+SawJay@users.noreply.github.com>
2024-04-11 15:37:44 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,12 @@
+// Display the message the user entered. 
+function displayMessage() {
+    let inputBox = document.getElementById('message');
+    let message = inputBox.value;
+
+    Swal.fire({
+        title: "App Name",
+        text: message,
+        icon: "success",
+        backdrop: false
+      });
+}
