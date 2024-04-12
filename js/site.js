e8f23441b165e94f5c050726d9a800d5a1499e15
SawJay <sawyerjd1@gmail.com>
2024-04-11 21:36:08 -0400

    TacoCat code until display

--- js/site.js
+++ js/site.js
@@ -1,12 +1,44 @@
 // Display the message the user entered. 
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+function getValues() {
 
-    Swal.fire({
-        title: "App Name",
-        text: message,
-        icon: "success",
-        backdrop: false
-      });
+  let originalString = document.getElementById('inputString').value;
+  // reverse the string
+  let revString = reverseString(originalString)
+  // check original to see if equals reverse
+  let isPalindrome = checkForPalindrome(originalString, revString);
+  // display the 3 values in a message
+  displayResults(isPalindrome, originalString, revString);
+
+}
+
+function reverseString (originalString) {
+
+  let reverseString = '';
+  
+  // radar
+  for (let index = originalString.length - 1; index >= 0; index--) {
+    
+    // r + a + d + a + r
+    reverseString += originalString[index];
+
+  }
+
+  return reverseString;
+}
+
+function checkForPalindrome(originalString, revString) {
+
+  if (originalString == revString) {
+    return true; 
+  } else {
+    return false;
+  }
+
+}
+
+function displayResults(isPalindrome, originalString, revString) {
+
+  document.getElementById('alert').classList.remove('invisible');
+  document.getElementById('msg').innerHTML = `Your string reversed is: <strong>${revString}</strong>`
+
 }
